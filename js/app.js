402116740d0f370037b8bf503b54a4f66620cc7a
AmaliaRiveraC <amaliariveracastillejos@gmail.com>
2017-05-03 15:35:48 +0000

    trabajando modal desde js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
 /*
  * Archivo principal de funcionalidad de JS
  */
+var imagenModal = document.getElementsByClassName("imagenes");
+var modal = document.getElementsByClassName("modal")[0];
+var imagen = document.getElementById("image");
+
+for(var i=0; i < imagenModal.length; i++) {
+    imagenModal[i].addEventListener("click", aparecerModal);
+}
+
+function aparecerModal () {
+    modal.style.display = "block";
+    imagen.src = this.src;
+}
+
