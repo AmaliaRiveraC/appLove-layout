6134d1d9a431aa0d6d308a539d9c1e1193808258
AmaliaRiveraC <amaliariveracastillejos@gmail.com>
2017-05-03 16:54:27 +0000

    trabajando funcionalidad de flechas modal

--- js/app.js
+++ js/app.js
@@ -4,13 +4,23 @@
 var imagenModal = document.getElementsByClassName("imagenes");
 var modal = document.getElementsByClassName("modal")[0];
 var imagen = document.getElementById("image");
+var atrasarImagen = document.getElementById("atrasarmodal");
+var adelantarImagen = document.getElementById("adelantarmodal");
 
 for(var i=0; i < imagenModal.length; i++) {
     imagenModal[i].addEventListener("click", aparecerModal);
 }
 
+for(var i=0; i < imagenModal.length) {
+    
+}
+
 function aparecerModal () {
     modal.style.display = "block";
     imagen.src = this.src;
+}
+
+function adelantarModal() {
+    for(var i=0)
 }
 
