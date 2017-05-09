088a40038368d3e2eb01086f964498381d020b8c
AmaliaRiveraC <amaliariveracastillejos@gmail.com>
2017-05-08 22:25:06 -0500

    ruta de imágenes en js en lugar de this.src

--- js/app.js
+++ js/app.js
@@ -6,19 +6,32 @@ var modal = document.getElementsByClassName("modal")[0];
 var imagen = document.getElementById("image");
 var atrasarModal = document.getElementById("atrasarModal");
 var adelantarModal = document.getElementById("adelantarModal");
+var n = 1;
 
 for(var i=0; i < imagenModal.length; i++) {
-    imagenModal[i].addEventListener("click", aparecerModal);
+    imagenModal[i].addEventListener("click", function() {
+    	imagen.src = "assets/" + "images/" + "img-" + i + ".jpg";
+    	modal.style.display = "block";
+    });
 }
 
-for(var i=0; i < imagenModal.length) {
-    
-}
 
-function aparecerModal () {
+
+	
+		/* for(var j=0; j < imagenModal) {
+			atrasarModal.addEventListener("click", function() {
+			imagen.src = "assets/" + "images/" + "img-" + [j -1] + ".jpg";
+			modal.style.display = "block";
+	});
+
+}*/
+
+
+
+/*function aparecerModal () {
     modal.style.display = "block";
     imagen.src = this.src;
-}
+}*/
 
 
 
